c6675731edfe742b5c3a5d243ee282e00f68c898
Oleg Borodatov <borolgs@gmail.com>
2020-08-06 21:04:08 +0300

    Fix config var

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { ConfigService } from '@nestjs/config';
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('jwtSecret'),
+        secret: configService.get('JWT_SECRET'),
         signOptions: { expiresIn: '1d' },
       }),
     }),

4f7172707344a16e03acca52f105980b7a09c827
Oleg Borodatov <borolgs@gmail.com>
2020-08-09 23:00:38 +0300

    Add full repo endpoint

--- src/scripts/scripts.controller.ts
+++ src/scripts/scripts.controller.ts
@@ -5,15 +5,24 @@ import {
   Request,
   UseInterceptors,
   CacheInterceptor,
+  Res,
+  Inject,
+  CACHE_MANAGER,
+  Headers,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { ScriptsService } from './scripts.service';
-import { Request as Req } from 'express';
+import { ScriptsService, MasterInfo } from './scripts.service';
+import { Request as Req, Response } from 'express';
+import { Cache } from 'cache-manager';
+import { Readable } from 'stream';
 
 @Controller()
 @UseGuards(JwtAuthGuard)
 export class ScriptsController {
-  constructor(private readonly scriptsService: ScriptsService) {}
+  constructor(
+    private readonly scriptsService: ScriptsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get('api/v1/scripts/*')
   @UseInterceptors(CacheInterceptor)
@@ -21,5 +30,52 @@ export class ScriptsController {
     const path = req.path.split('api/v1/scripts/')[1];
     const data = await this.scriptsService.get(path);
     return data;
+  }
+
+  @Get('api/v1/scripts')
+  async getAll(
+    @Res() res: Response,
+    @Headers('Scripts-Sha') userSha: string,
+  ): Promise<any> {
+    let serverMasterInfo: MasterInfo = await this.cacheManager.get('sha');
+    if (!serverMasterInfo) {
+      const info = await this.scriptsService.getMasterInfo();
+      await this.cacheManager.set('sha', info, { ttl: 360 });
+      serverMasterInfo = info;
+    }
+
+    if (serverMasterInfo.sha === userSha) {
+      return res.status(204).send();
+    }
+
+    const dataFromCache = await this.cacheManager.get('master');
+    if (dataFromCache && dataFromCache.info.sha === serverMasterInfo.sha) {
+      return this.createResponse(res, dataFromCache);
+    }
+
+    const { buffer } = await this.scriptsService.getFullRepo();
+    const data = { buffer, info: serverMasterInfo };
+
+    await this.cacheManager.set('master', data, { ttl: 60 * 60 * 24 });
+
+    return this.createResponse(res, data);
+  }
+
+  private async createResponse(
+    res: Response,
+    data: { buffer: Buffer; info: MasterInfo },
+  ) {
+    const { buffer, info } = data;
+
+    const stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+
+    res.set({
+      'Content-Type': 'application/zip',
+      'Content-Length': buffer.length,
+      'Scripts-Sha': info.sha,
+    });
+    return stream.pipe(res);
   }
 }
--- src/scripts/scripts.service.ts
+++ src/scripts/scripts.service.ts
@@ -1,6 +1,17 @@
 import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
 import fetch from 'node-fetch';
 import { ConfigService } from '@nestjs/config';
+import JSZip from 'jszip';
+import path from 'path';
+
+export type MasterInfo = {
+  sha: string;
+  message: string;
+};
+
+export type AllScripts = {
+  buffer: Buffer;
+};
 
 @Injectable()
 export class ScriptsService {
@@ -49,5 +60,82 @@ export class ScriptsService {
     };
 
     return fileObj;
+  }
+
+  async getFullRepo(): Promise<AllScripts> {
+    const buffer = await this.getMasterBuffer();
+    const newBuffer = await this.createNewMasterBuffer(buffer);
+
+    return {
+      buffer: newBuffer,
+    };
+  }
+
+  async getMasterInfo(): Promise<MasterInfo> {
+    const response = await fetch(
+      `https://api.github.com/repos/${this.userName}/${this.repo}/commits/master`,
+      {
+        method: 'get',
+        headers: {
+          Authorization: `token ${this.token}`,
+        },
+      },
+    );
+
+    if (response.status !== HttpStatus.OK) {
+      throw new HttpException('Error', response.status);
+    }
+
+    const { sha, commit } = await response.json();
+
+    const masterInfo = {
+      sha,
+      message: commit.message,
+    };
+
+    return masterInfo;
+  }
+
+  private async getMasterBuffer(): Promise<Buffer> {
+    const response = await fetch(
+      `https://api.github.com/repos/${this.userName}/${this.repo}/zipball/master`,
+      {
+        method: 'get',
+        headers: {
+          Authorization: `token ${this.token}`,
+        },
+      },
+    );
+
+    if (response.status !== HttpStatus.OK) {
+      throw new HttpException('Error', response.status);
+    }
+
+    const buffer = await response.buffer();
+    return buffer;
+  }
+
+  private async createNewMasterBuffer(buffer: Buffer): Promise<Buffer> {
+    const zip = await JSZip.loadAsync(buffer);
+    const files = zip.files;
+    const filepaths = Object.keys(files);
+
+    const newZip = new JSZip();
+
+    for (const filepath of filepaths) {
+      const allowedExts = ['.py', '.xaml'];
+      const isAllowedFile =
+        filepath.includes('src/scripts') &&
+        allowedExts.includes(path.parse(filepath).ext);
+      if (!isAllowedFile) continue;
+      const zipObj = zip.file(filepath);
+      const buffer = await zipObj.async('nodebuffer');
+
+      newZip.file(filepath, buffer);
+    }
+
+    const newBuffer = await newZip.generateAsync({ type: 'nodebuffer' });
+
+    return newBuffer;
   }
 }
